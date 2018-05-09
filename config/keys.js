2e39d836292bcc45562579f747650c87dfa20b7c
Silas Burger <ybsilas@gmail.com>
2018-05-09 15:44:04 -0700

    Added files via upload

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    "269917058201-pf5untrnoqp4ddqs8omiljesf8ds274m.apps.googleusercontent.com",
+  googleClientSecret: "cnSED0T0qXZNkwXVJVOTp9_T",
+  mongoURI:
+    "mongodb://mangomassacre:mAlai0-mama@ds119660.mlab.com:19660/emaile-dev"
+};
